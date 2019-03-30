c891b4b39d1d21a26d025188f31f5169cc4b12af
LuisMiguel <lm.gomezl@uniandes.edu.co>
2019-03-30 16:13:20 -0500

    Tarjeta ya funcional

--- src/app/tarjeta/tarjeta-list/tarjeta-list.component.ts
+++ src/app/tarjeta/tarjeta-list/tarjeta-list.component.ts
@@ -1,31 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Tarjeta } from '../tarjeta';
 import { TarjetaService } from '../tarjeta.service';
 import { SELECT_VALUE_ACCESSOR } from '@angular/forms/src/directives/select_control_value_accessor';
 
-@Component({
-  selector: 'app-tarjeta',
-  templateUrl: './tarjeta-list.component.html',
-  styleUrls: ['./tarjeta-list.component.css']
+@Component ({
+    selector: 'app-tarjeta',
+    templateUrl: './tarjeta-list.component.html',
+    styleUrls: ['./tarjeta-list.component.css']
 })
 export class TarjetaListComponent implements OnInit {
 
-  constructor(
-    private tarjetaService: TarjetaService
-    ) { }
+    /**
+     * Constructor for the component
+     * @param tarjetaoService The author's services provider
+     */
+    constructor(private tarjetaService: TarjetaService) {}
 
-  tarjetas: Tarjeta[];
+    /**
+     * The list of tarjetas which belong to the BookStore
+     */
+    tarjetas: Tarjeta[];
 
-  getTarjetas(): void {
-    this.tarjetaService.getTarjetas()
-        .subscribe(tarjetas => {
-          this.tarjetas = tarjetas;
-        });
-        console.log(this.tarjetas);
-  }
+    /**
+     * Asks the service to update the list of tarjetas
+     */
+    getTarjetas(): void {
+        this.tarjetaService.getTarjetas()
+            .subscribe(tarjeta => this.tarjetas = tarjeta);
+    }
 
-  ngOnInit() {
-    this.getTarjetas;
-  }
-
+    /**
+     * This will initialize the component by retrieving the list of tarjetas from the service
+     * This method will be called when the component is created
+     */
+    ngOnInit() {
+        this.getTarjetas();
+    }
 }
