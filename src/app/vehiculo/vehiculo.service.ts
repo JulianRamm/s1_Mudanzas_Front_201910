9ddd6a088025575acfec4fdfdd920098fc9eaa5e
Samuel Bernal Neira <s.bernaln@uniandes.edu.co>
2019-04-10 07:22:40 -0500

    vehiculo

---
+++ src/app/vehiculo/vehiculo.service.ts
@@ -0,0 +1,39 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+ 
+ import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+
+import {Vehiculo} from './vehiculo';
+import { environment } from '../../environments/environment';
+
+const API_URL = environment.apiURL;
+const vehiculos = '/vehiculos';
+
+/**
+* The service provider for everything related to vehiculos
+*/
+@Injectable()
+export class VehiculoService {
+
+    /**
+    * Constructor of the service
+    * @param http The HttpClient - This is necessary in order to perform requests
+    */
+    constructor(private http: HttpClient) {}
+
+    /**
+    * Returns the Observable object containing the list of vehiculos retrieved from the API
+    * @returns The list ofvehiculos in real time
+    */
+    getVehiculos(): Observable<Vehiculo[]> {
+        return this.http.get<Vehiculo[]>(API_URL + vehiculos);
+    }
+
+}
+
+
